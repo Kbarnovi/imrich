1454d76f6e2079b4ca6e729160edb68aff6436ca
kbarnovi <kbarnovi1@gmail.com>
2025-04-18 17:41:41 +0400

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Ketevan Barnovi on 18.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
